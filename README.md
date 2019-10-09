e3ee9c9e60f1019da31f3c1ff85ef62e4b7c336a
Emil9999 <emil.sallem@code.berlin>
2019-10-09 13:10:00 +0200

    Basic db setup

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 ROWA
+
+npm install
+
+go get github.com/labstack/echo
+go get github.com/labstack/echo/middleware
+
+npm run dev
+go run main.go
--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type User struct {
@@ -24,6 +27,7 @@ func plant(c echo.Context) (err error) {
 	return ret
 }
 func main() {
+	dbSetup()
 	// Echo instance
 	e := echo.New()
 
@@ -34,4 +38,23 @@ func main() {
 	e.POST("/plant/greet", plant)
 	// Start server
 	e.Logger.Fatal(e.Start(":3000"))
+}
+func dbSetup() {
+	//Opening DB
+	database, _ := sql.Open("sqlite3", "./rowa.db")
+
+	//Creating
+	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS PlantType (Id INTEGER PRIMARY KEY, Name TEXT, GrowthTime INTEGER)")
+	statement.Exec()
+	statement, _ = database.Prepare("INSERT INTO PlantType (Name, Growthtime) VALUES (?, ?)")
+
+	statement.Exec("Lettuce", "42")
+	rows, _ := database.Query("SELECT Id, Name, Growthtime from PlantType")
+	var name string
+	var growthTime int
+	var id int
+	for rows.Next() {
+		rows.Scan(&id, &name, &growthTime)
+		fmt.Println(name + ": " + strconv.Itoa(growthTime))
+	}
 }
