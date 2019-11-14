e7917c3e368730eb23feabe4e34fa93862ee2d38
hannesostwaldcode <hannes.ostwald@code.berlin>
2019-11-14 14:43:47 +0100

    Starting creating Farming vue

---
+++ frontend/src/components/farming/FarmingTopRow.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-row>
+        <v-col cols="4" style="padding-left: 30px">
+            <v-btn dark fab color="white">
+                <v-icon color="primary">mdi-arrow-left</v-icon>
+            </v-btn>
+        </v-col>
+        <v-col align-content-center cols="4">
+           
+        </v-col> 
+        <v-col cols="4" style="float:right">
+            <v-btn dark fab color="white">
+                <v-icon color="primary">mdi-close</v-icon>
+            </v-btn>
+        </v-col>
+    </v-row>
+
+</template>
+
+<script>
+
+    export default {
+        name: "FarmingTopRow",
+        components:{
+        
+        }
+    }
+</script>
+
+<style scoped>
+    #logo-text{
+        font-family: Montserrat, sans-serif;
+        font-style: normal;
+        font-weight: 600;
+        font-size: 36px;
+        color: var(--v-primary-base)
+    }
+</style>
