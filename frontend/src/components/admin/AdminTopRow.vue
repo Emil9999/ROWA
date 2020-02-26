3eef4a3219c93838096fd46ff79e765a5b1bc948
Hannes Ostwald <hannes.ostwald@code.berlin>
2020-02-26 15:27:54 +0100

    finished frontend Lightsettings

---
+++ frontend/src/components/admin/AdminTopRow.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-row align-center>
+        <v-col cols="2" style="padding-left: 30px">
+             <router-link :to="{}">
+            <v-btn dark fab color="white">
+                <v-icon color="primary">mdi-arrow-left</v-icon>
+            </v-btn>
+             </router-link>
+        </v-col>
+        <v-col align="center"  align-self="center" cols="8">
+             <h3 style="color:#828282" v-text="headtext"></h3>
+        </v-col>
+        <v-col cols="2" id="logo-text" style="text-align: right; padding-right: 30px">
+             <router-link :to="{name:'Home'}">
+            <v-btn dark fab color="white">
+                <v-icon color="primary">mdi-close</v-icon>
+            </v-btn>
+            </router-link>
+        </v-col>
+    </v-row>
+
+</template>
+
+<script>
+    
+    export default {
+        name: "AdminTopRow",
+        components:{
+            
+        },
+        props:{
+                headtext: String
+        }
+    }
+</script>
+
+<style scoped>
+    #logo-text{
+        font-family: Montserrat, sans-serif;
+        font-style: normal;
+        font-weight: 600;
+        font-size: 36px;
+        color: var(--v-primary-base)
+    }
+</style>
--- frontend/src/views/AdminSettings.vue
+++ frontend/src/views/AdminSettings.vue
@@ -1,32 +1,19 @@
 <template>
     <div id="Admin">
-<v-btn dark fab color="white" :to="{name:'Home'}">
-                <v-icon color="primary">mdi-close</v-icon>
-            </v-btn>
+        <AdminTopRow v-bind:headtext="'Settings'"></AdminTopRow>
 
-        <h1>Plan your event:</h1>
-      <v-row justify="space-around" align="center">
-        <v-col style="width: 290px; flex: 0 1 auto;">
-          <h2>Light On:</h2>
-          <h3>Currently: {{current_time.time_on}}</h3>
-          <v-time-picker v-model="start" :max="end"></v-time-picker>
-        </v-col>
-        <v-col style="width: 290px; flex: 0 1 auto;">
-          <h2>Light Off: </h2>
-          <h3>Currently: {{current_time.time_off}}</h3>
-          <v-time-picker v-model="end" :min="start"></v-time-picker>
-        </v-col>
-      </v-row>
+        <v-row justify="center" class="info-box" margin="auto" padding="auto">
+            <h1>E-Mail</h1>
+         </v-row>
 
-     <v-btn id="button"   :disabled="TimeDisable" @click="sendLightTimes()" rounded color="accent" height="75" width="360">
-           Save
-            <v-icon right dark>mdi-checkbox-marked-circle</v-icon>
-          </v-btn>
-     <v-switch v-model="current_time.state" @change="changeLight()">
-        <template v-slot:label>
-        Light Switch
-        </template>
-      </v-switch>
+         <v-row justify="center" class="info-box" margin="auto" padding="auto">
+             <TimeSettings></TimeSettings>
+         </v-row>
+
+         <v-row justify="center" class="info-box" margin="auto" padding="auto">
+             <h1> Pin </h1>
+         </v-row>
+      
     
     
     </div>
@@ -34,84 +21,33 @@
 
 
 <script>
-    import axios from "axios"
+    
+    import AdminTopRow from "@/components/admin/AdminTopRow.vue"
+    import TimeSettings from "@/components/admin/TimeSettings.vue"
 
     export default {
         name: "AdminSettings",
         components: {
+            AdminTopRow,
+            TimeSettings
            
         },
          data(){
              return{
-            lightStatus: false,
-            start: null,
-            end: null,
-            current_time: {
-                    time_on: null,
-                    time_off: null,
-                    state: null
-                },
+
              };
          },
          methods: {
-              getLightTimes: function () {
-                axios.get("http://127.0.0.1:3000/adminSettings/get-light")
-                    .then(result => {
-                        this.current_time.time_on = result.data.time_on
-                        this.current_time.time_off = result.data.time_off
-                        this.current_time.state = !!result.data.state
-                        /*if (result.data.state == 1){
-                            this.current_time.state =  true
-                        }else{
-                            this.current_time.state  = false
-                        }
-                        */
-                    })
-                    .catch(error => {
-                        console.log(error)
-                    })
-            },
+              
 
-            sendLightTimes:function () {
-                axios.post("http://127.0.0.1:3000/adminSettings/insert-light",
-                    {time_on: this.start, time_off: this.end},
-                    "content-type: application/json")
-                    .then()
-                    .catch(error => {
-                        console.log(error);
-                    });
-
-            },
-             changeLight:function () {
-                console.log(this.time_on)
-                axios.post("http://127.0.0.1:3000/adminSettings/changelight",
-                    {state: this.current_time.state| 0},
-                    "content-type: application/json")
-                    .then()
-                    .catch(error => {
-                        console.log(error);
-                    }); 
-
-            },
 
          },
-         created() {
-             this.getLightTimes()
-         },
-         
-         computed: {
-         TimeDisable: function() {
-            if((this.start == null) || (this.end == null)){
-            return true
-            }
-            else{
-            return false
-            }
-        }
+        
+        
         
 
 
-         }
+         
     };
 </script>
 
@@ -146,4 +82,11 @@
         background-color: transparent;
         text-decoration: none;
     }
+
+    .info-box {
+  background: #ffffff;
+  border-radius: 10px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  margin: 40px 50px 0 50px;
+}
 </style>
