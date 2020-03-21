21865fcf716e7d0e08fd46501f176bdb42ef54ac
Emil9999 <emil.sallem@code.berlin>
2020-03-21 19:06:13 +0100

    Added cloud connection and push sensor data

--- backend/src/influx/sensor_db.go
+++ backend/src/influx/sensor_db.go
@@ -1,17 +1,20 @@
-package db
+package influx
 
 import (
 	"context"
 	"log"
+	"net/http"
 	"time"
 
 	"github.com/influxdata/influxdb-client-go"
 )
 
 func InitInflux() *influxdb.Client {
-
+	c := &http.Client{}
 	token := "Ap_gylHmGd77dD86tHwH1PDlGpeXoz0HqotUWO43-cLNKMAA_K_QtY2-cqvCf6CSviagvPotqiSqalrQ_Ie3EQ=="
-	influx, err := influxdb.New("https://eu-central-1-1.aws.cloud2.influxdata.com", token, influxdb.WithHTTPClient(nil))
+
+	influx, err := influxdb.New("https://eu-central-1-1.aws.cloud2.influxdata.com", token, influxdb.WithHTTPClient(c))
+	log.Println(err)
 	if err != nil {
 		panic(err) // error handling here; normally we wouldn't use fmt but it works for the example
 	}
@@ -19,13 +22,21 @@ func InitInflux() *influxdb.Client {
 	return influx
 
 }
-func InfluxWrite(temp float32, lightIntensity float32, influx influxdb.Client) {
+func InfluxWrite(s []float32, datetime time.Time, influx *influxdb.Client) {
+	//now := time.Now()
 	data := []influxdb.Metric{
 		influxdb.NewRowMetric(
-			map[string]interface{}{"temp": temp, "lightIntensity": lightIntensity},
-			"system-metrics",
-			map[string]string{"hostname": "hal9000"},
-			time.Date(2018, 3, 4, 5, 6, 7, 8, time.UTC)),
+			map[string]interface{}{
+				"temp":           s[0],
+				"lightIntensity": s[1],
+				"humidity":       s[2],
+				"waterLevel":     s[3],
+				"waterTemp":      s[4],
+				"waterpH":        s[5],
+			},
+			"sensor-data",
+			map[string]string{"farm-id": "1"},
+			datetime),
 	}
 	// The actual write..., this method can be called concurrently.
 	if _, err := influx.Write(context.Background(), "sensordata", "170189425a059be1", data...); err != nil {
@@ -33,6 +44,6 @@ func InfluxWrite(temp float32, lightIntensity float32, influx influxdb.Client) {
 	}
 }
 
-func InfluxClose(c influxdb.Client) {
+func InfluxClose(c *influxdb.Client) {
 	c.Close() // closes the client.  After this the client is useless.
 }
--- backend/src/sensor/sensors.go
+++ backend/src/sensor/sensors.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/MarcelCode/ROWA/src/db/sensor_db"
+	"github.com/MarcelCode/ROWA/src/influx"
 	"github.com/tarm/serial"
 )
 
@@ -96,23 +96,32 @@ func ReadFakeSensorData() {
 	database, _ := sql.Open("sqlite3", "./rowa.db")
 	statement, _ := database.Prepare("INSERT OR IGNORE INTO SensorMeasurements (Datetime, Temp, LightIntensity, Humidity, WaterLevel, WaterTemp, WaterpH) VALUES (?, ?, ?, ?, ?, ?, ?)")
 	defer database.Close()
+	client := influx.InitInflux()
+
 	for {
-		datetime := time.Now().UTC().Format(time.RFC3339)
+		var s []float32
+		datetime := time.Now()
 		temp := rand.Float32()*2 + 20
 		lightIntensity := rand.Float32()*100 + 400
 		humidity := rand.Float32()*10 + 30
 		waterLevel := rand.Float32()*4 + 19
 		waterTemp := rand.Float32()*5 + 18
 		waterpH := rand.Float32()*2 + 6
-		fmt.Println(datetime, temp, lightIntensity, humidity, waterLevel, waterTemp, waterpH)
-		statement.Exec(datetime, temp, lightIntensity, humidity, waterLevel, waterTemp, waterpH)
+		s = append(s, temp, lightIntensity, humidity, waterLevel, waterTemp, waterpH)
+		influx.InfluxWrite(s, datetime, client)
+		datetimeStr := datetime.UTC().Format(time.RFC3339)
+		fmt.Println(datetimeStr, temp, lightIntensity, humidity, waterLevel, waterTemp, waterpH)
+		statement.Exec(datetimeStr, temp, lightIntensity, humidity, waterLevel, waterTemp, waterpH)
 
 		//Cloud part
-		client := sensor_db.InitInflux()
-		sensor_db.InfluxWrite(25.0, 500.0, client)
-		sensor_db.InfluxClose(client)
+
+		//str := "mem,host=host1 used_percent=23.43234543 1556896326"
+		//url := "https://eu-central-1-1.aws.cloud2.influxdata.com/api/v2/write?org=170189425a059be1&bucket=sensordata&precision=s"
+		//req, err := http.NewRequest("POST", url, bytes.NewBuffer(str))
 		time.Sleep(60 * time.Second)
 	}
+	//TODO on system shutdown influx.InfluxClose(client)
+
 }
 func ReadSensorData() {
 	var serialString string
