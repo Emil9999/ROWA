2283e7f5cb1043a58b20ab39e7987b98ab4e80fe
Emil9999 <emil.sallem@code.berlin>
2020-04-22 20:58:36 +0100

    Added sudo to run script

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo Starting backend
-docker run --publish 3000:3000 --name backend_x --rm --device=/dev/ttyACM0 -e AWS_ACCESS_KEY_ID=AKIAUJHOO43UQH4ZR7E2 -e AWS_SECRET_ACCESS_KEY=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 backend 
+sudo docker run --publish 3000:3000 --name backend_x --rm --device=/dev/ttyACM0 -e AWS_ACCESS_KEY_ID=AKIAUJHOO43UQH4ZR7E2 -e AWS_SECRET_ACCESS_KEY=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 backend 
 echo Starting frontend
-docker run --publish 80:80 --name frontend_x --rm frontend
+sudo docker run --publish 80:80 --name frontend_x --rm frontend
