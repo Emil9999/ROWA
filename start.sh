ec7f0c644b060cfbeb5df625a109ba06c314e18e
Emil9999 <emil.sallem@code.berlin>
2020-04-23 02:19:50 +0100

    Enabled cors on nginx

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sudo date -s "$(curl -s --head http://google.com | grep ^Date: | sed 's/Date: //g') +0100"
 echo Starting backend
 sudo docker run --publish 3000:3000 --name backend_x --rm --device=/dev/ttyACM0 -e AWS_ACCESS_KEY_ID=AKIAUJHOO43UQH4ZR7E2 -e AWS_SECRET_ACCESS_KEY=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 backend 
 echo Starting frontend
