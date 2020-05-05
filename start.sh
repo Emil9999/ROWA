a2449f6c2135791b879c7e1a9854c7c326bd2747
Emil9999 <55983360+Emil9999@users.noreply.github.com>
2020-05-05 22:14:53 +0200

    Delete start.sh

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo Starting backend
-sudo docker run --publish 3000:3000 --name backend_x --rm --device=/dev/ttyACM0 -e AWS_ACCESS_KEY_ID=AKIAUJHOO43UQH4ZR7E2 -e AWS_SECRET_ACCESS_KEY=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 backend 
-echo Starting frontend
-sudo docker run --publish 80:80 --name frontend_x --rm frontend
