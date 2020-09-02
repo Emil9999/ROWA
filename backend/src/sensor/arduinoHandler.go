9ddf87c206761c208ba1d98abd57d056f7226bda
Emil9999 <emil.sallem@code.berlin>
2020-09-02 13:26:13 +0200

    swtiched queue

--- backend/src/sensor/arduinoHandler.go
+++ backend/src/sensor/arduinoHandler.go
@@ -1,14 +1,18 @@
 package sensor
 
 import (
+	"container/list"
+	"fmt"
 	"log"
 	"time"
 )
 
 var ch = make(chan string, 100)
+var queue = list.New()
 
 func WriteToCh(input string) {
 	ch <- input
+	queue.PushBack(input)
 }
 
 func ArduinoLoop() {
@@ -19,20 +23,31 @@ func ArduinoLoop() {
 	defer s.Close()
 	log.Println("Starting loop")
 	_, err = s.Write([]byte("80"))
-	time.Sleep(time.Second * 2)
+	time.Sleep(time.Second * 1)
 	_, err = s.Write([]byte("81"))
-	time.Sleep(time.Second * 2)
+	time.Sleep(time.Second * 1)
 	_, err = s.Write([]byte("80"))
-	time.Sleep(time.Second * 2)
-	_, err = s.Write([]byte("81"))
+	time.Sleep(time.Second * 1)
 
 	for {
-		for v := range ch {
+		/*for v := range ch {
 			log.Println("Writing ", v, "to arduino")
 			_, err = s.Write([]byte(v))
 			if err != nil {
 				log.Fatal(err)
 			}
+			time.Sleep(2 * time.Second)
+
+		}*/
+		for queue.Len() > 0 {
+			v := queue.Front()
+			str := fmt.Sprintf("%v", v)
+			log.Println("Writing ", str, "to arduino")
+			_, err = s.Write([]byte(str))
+			if err != nil {
+				log.Fatal(err)
+			}
+			queue.Remove(v)
 			time.Sleep(2 * time.Second)
 
 		}
