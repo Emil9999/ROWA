a7ed9963cefcb40ffdb118a50b32654111e57521
hannesostwaldcode <hannes.ostwald@code.berlin>
2020-11-12 18:38:26 +0100

    added fullstory

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,8 +4,12 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
 import Vue2TouchEvents from 'vue2-touch-events'
+import * as FullStory from '@fullstory/browser';
 
 Vue.use(Vue2TouchEvents)
+
+FullStory.init({  orgId: 'ZAA45'});
+Vue.prototype.$FullStory =  FullStory;
 
 Vue.config.productionTip = false
 
