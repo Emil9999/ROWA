5ebf235e28b4985cb4907a0a16a545f210cac8bd
Emil9999 <emil.sallem@code.berlin>
2021-02-05 14:14:59 +0100

    changed imprint

--- backend/src/sensor/lightHandler.go
+++ backend/src/sensor/lightHandler.go
@@ -19,14 +19,15 @@ func BlinkLight() {
 
 	pin := rpio.Pin(19)
 	pin.Output()
+	pin.High()
 	pin.Mode(rpio.Pwm)
-	pin.Freq(1000)
+	pin.Freq(100)
 	pin.DutyCycle(0, 32)
 	// the LED will be blinking at 2000Hz
 	// (source frequency divided by cycle length => 64000/32 = 2000)
 
 	// five times smoothly fade in and out
-	for i := 0; i < 5; i++ {
+	for i := 0; i < 10; i++ {
 		for i := uint32(0); i < 32; i++ { // increasing brightness
 			pin.DutyCycle(i, 32)
 			time.Sleep(time.Second / 32)
