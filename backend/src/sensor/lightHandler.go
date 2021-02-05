2bb23a34a58d1b4aaacc4ef31e5b4741cb81b81a
Emil9999 <emil.sallem@code.berlin>
2021-02-05 12:18:11 +0100

    added triggerpump test func

---
+++ backend/src/sensor/lightHandler.go
@@ -0,0 +1,5 @@
+package sensor
+
+func SwitchLight() {
+
+}
--- backend/src/sensor/pumpHandler.go
+++ backend/src/sensor/pumpHandler.go
@@ -1,7 +1,12 @@
+package sensor
+
 import "github.com/stianeikeland/go-rpio/v4"
 
 func TriggerPumpX() {
 	err := rpio.Open()
+	if err != nil {
+		Fatal.log(err)
+	}
 	pin := rpio.Pin(29)
 	pin.Toggle()
 }
