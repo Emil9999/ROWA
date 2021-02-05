a735c1ec99ebb9834a44cb254fb09be2a716e159
Emil9999 <emil.sallem@code.berlin>
2021-02-05 12:49:49 +0100

    changed pin

--- backend/src/sensor/pumpHandler.go
+++ backend/src/sensor/pumpHandler.go
@@ -1,12 +1,21 @@
 package sensor
 
-import "github.com/stianeikeland/go-rpio/v4"
+import (
+	"time"
+
+	"github.com/stianeikeland/go-rpio/v4"
+)
 
 func TriggerPumpX() {
 	err := rpio.Open()
 	if err != nil {
 		Fatal.log(err)
 	}
-	pin := rpio.Pin(29)
-	pin.Toggle()
+	defer rpio.Close()
+	pin.Output()
+	pin := rpio.Pin(2)
+	for x := 0; x < 20; x++ {
+		pin.Toggle()
+		time.Sleep(time.Second / 5)
+	}
 }
