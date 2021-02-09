c012a5d6a62404d0c9b6e2f3fd9406f2dcdea43f
Emil9999 <emil.sallem@code.berlin>
2021-02-09 14:10:16 +0100

    changed imprint

--- backend/src/sensor/weightSensor.go
+++ backend/src/sensor/weightSensor.go
@@ -23,20 +23,22 @@ func ReadWeight() {
 	var weight2 float64
 
 	weight1 = 1000
-	weight2 = 2000*/
+	weight2 = 2000
 
-	//hx711.GetAdjustValues(weight1, weight2)
+	hx711.GetAdjustValues(weight1, weight2)*/
 	// SetGain default is 128
 	// Gain of 128 or 64 is input channel A, gain of 32 is input channel B
 	//hx711.SetGain(128)
 
 	// make sure to use your values from calibration above
 	//hx711.AdjustZero = -185820
-	hx711.AdjustZero, _ = hx711.ReadDataMedianRaw(11)
+	/*hx711.AdjustZero, _ = hx711.ReadDataMedianRaw(11)
 
 	hx711.AdjustScale = 1050
 	var data float64
-	for i := 0; i < 10000; i++ {
+	previousReadings := []float64{}
+
+	for i := 0; i < 12; i++ {
 		time.Sleep(200 * time.Microsecond)
 
 		data, err = hx711.ReadDataMedian(11)
@@ -44,7 +46,34 @@ func ReadWeight() {
 			fmt.Println("ReadDataMedian error:", err)
 			continue
 		}
-
-		fmt.Println(data)
+		previousReadings = previousReadings.append(data)
+		//fmt.Println(data)
 	}
+	movingAvg, err := hx711.ReadDataMedianThenMovingAvgs(11, 8, &previousReadings)
+	if err != nil {
+		fmt.Println("ReadDataMedianThenMovingAvgs error:", err)
+	}
+
+	// moving average
+	fmt.Println(movingAvg)*/
+
+	hx711.AdjustZero, _ = hx711.ReadDataMedianRaw(11)
+
+	hx711.AdjustScale = 1050
+	var movingAvg float64
+	stop := false
+	stopped = make(chan struct{})
+	go hx711.BackgroundReadMovingAvgs(11, 8, &movingAvg, &stop, stopped)
+
+	// wait for data
+	time.sleep(time.Second)
+
+	// moving average
+	fmt.Println(movingAvg)
+
+	// when done set stop to true to quit BackgroundReadMovingAvgs
+	stop = true
+
+	// wait for BackgroundReadMovingAvgs to stop
+	<-stopped
 }
