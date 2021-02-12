973a6951058dbbd1049aa3162db7b28b46be9c6a
Emil9999 <emil.sallem@code.berlin>
2021-02-12 18:21:46 +0100

    old light handler

--- backend/src/sensor/lightHandler.go
+++ backend/src/sensor/lightHandler.go
@@ -21,13 +21,13 @@ func BlinkLight(pin int64, toggle bool) {
 	time.Sleep(time.Second * 2)
 	if toggle {
 		for {
-			for i := 20; i < 100; i++ { // increasing brightness
+			for i := 12; i < 100; i++ { // increasing brightness
 				b.ApplyBlaster(pin, float64(i)/100)
-				time.Sleep(time.Millisecond * 10)
+				time.Sleep(time.Millisecond * 30)
 			}
-			for i := 100; i > 20; i-- { // decreasing brightness
+			for i := 100; i > 12; i-- { // decreasing brightness
 				b.ApplyBlaster(pin, float64(i)/100)
-				time.Sleep(time.Millisecond * 10)
+				time.Sleep(time.Millisecond * 30)
 
 			}
 		}
