0362085cd549af69e92d3edf741fcfb7dc476a9c
Emil9999 <emil.sallem@code.berlin>
2021-02-26 12:43:09 +0100

    weight

--- backend/src/sensor/weightSensor.go
+++ backend/src/sensor/weightSensor.go
@@ -19,22 +19,22 @@ func ReadWeight() {
 		fmt.Println("NewHx711 error:", err)
 		return
 	}
-	var weight1 float64
+	/*var weight1 float64
 	var weight2 float64
 
 	weight1 = 1000
 	weight2 = 2000
 
-	hx711.GetAdjustValues(weight1, weight2)
+	hx711.GetAdjustValues(weight1, weight2)*/
 	// SetGain default is 128
 	// Gain of 128 or 64 is input channel A, gain of 32 is input channel B
 	//hx711.SetGain(128)
 
 	// make sure to use your values from calibration above
 	//hx711.AdjustZero = -185820
-	//hx711.AdjustZero, _ = hx711.ReadDataMedianRaw(11)
+	hx711.AdjustZero, _ = hx711.ReadDataMedianRaw(11)
 
-	//hx711.AdjustScale = 1050
+	hx711.AdjustScale = 105
 	//var data float64
 	for i := 0; i < 12000; i++ {
 		time.Sleep(200 * time.Microsecond)
