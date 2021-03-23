3ed552e64da3b8fbf5d8791a55557319d01860f2
Emil9999 <emil.sallem@code.berlin>
2021-03-23 13:05:21 +0100

    c

--- backend/src/sensor/lightModule.go
+++ backend/src/sensor/lightModule.go
@@ -46,7 +46,8 @@ func (lm *Module) init() {
 
 func (ms *ModulesStruct) SetPinsHigh(pin int) {
 	for id, module := range ms.Modules {
-		if id != pin {
+		fmt.Println(id, module)
+		/*if id != pin {
 			p := gpioreg.ByName("GPIO" + strconv.Itoa(pin))
 			if err := p.Out(gpio.High); err != nil {
 				log.Fatal(err)
@@ -57,7 +58,7 @@ func (ms *ModulesStruct) SetPinsHigh(pin int) {
 			if err := p.Out(gpio.Low); err != nil {
 				log.Fatal(err)
 			}
-		}
+		}*/
 
 	}
 }
