a6528aca5b23e22259b248f2071a71dcee82ac9c
Emil9999 <emil.sallem@code.berlin>
2021-03-26 13:57:04 +0100

    added temp with periph

--- backend/src/sensor/waterTempSensor.go
+++ backend/src/sensor/waterTempSensor.go
@@ -2,21 +2,32 @@ package sensor
 
 import (
 	"fmt"
-	"log"
 
-	dht "github.com/d2r2/go-dht"
+	dht "github.com/MichaelS11/go-dht"
 	"github.com/yryz/ds18b20"
 )
 
 func ReadDht() {
-	temperature, humidity, retried, err :=
-		dht.ReadDHTxxWithRetry(dht.DHT11, 4, false, 10)
+	err := dht.HostInit()
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("HostInit error:", err)
+		return
 	}
-	// Print temperature and humidity
-	fmt.Printf("Temperature = %v*C, Humidity = %v%% (retried %d times)\n",
-		temperature, humidity, retried)
+
+	dht, err := dht.NewDHT("GPIO4", dht.Fahrenheit, "")
+	if err != nil {
+		fmt.Println("NewDHT error:", err)
+		return
+	}
+
+	humidity, temperature, err := dht.ReadRetry(11)
+	if err != nil {
+		fmt.Println("Read error:", err)
+		return
+	}
+
+	fmt.Printf("humidity: %v\n", humidity)
+	fmt.Printf("temperature: %v\n", temperature)
 }
 
 func ReadTemp() (temp float64) {
