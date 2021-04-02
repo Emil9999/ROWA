57189ea028d05c14acfbb9ca3ea0cd8757b3afad
Emil9999 <emil.sallem@code.berlin>
2021-04-02 13:40:08 +0200

    revert to vue2

---
+++ frontend/src/components/TopHomeButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <a v-if="href" :href="href" class="button">
+      <slot/>
+    </a>
+    <button v-else class="button">
+      <slot/>
+    </button>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      href: {
+        type: String,
+        default: null
+      }
+    }
+  }
+</script>
