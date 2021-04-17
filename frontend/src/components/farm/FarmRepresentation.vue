49d1555b2165b5602f8c0af33c95c9816f28dd0e
Emil9999 <emil.sallem@code.berlin>
2021-04-17 14:17:59 +0200

    added farm rep and module html/css

---
+++ frontend/src/components/farm/FarmRepresentation.vue
@@ -0,0 +1,46 @@
+<template>
+    <div style="position: relative">
+        <svg width="410" height="522" viewBox="0 0 410 522" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect x="190" width="30" height="494" fill="#BDBDBD"/>
+            <path d="M0 522L0 494H410V522H0Z" fill="#BDBDBD"/>
+            <rect width="190" height="7" transform="matrix(-1 0 0 1 190 0)" fill="#BDBDBD"/>
+            <rect width="190" height="7" transform="matrix(-1 0 0 1 410 0)" fill="#BDBDBD"/>
+        </svg>
+        <div class="module" style="bottom: 371px; right: 0px;">
+        <farm-module/>
+        </div>
+         <div class="module" style="bottom: 325px;">
+        <farm-module/>
+        </div>
+         <div class="module" style="bottom: 300px; right: 0px;">
+        <farm-module/>
+        </div>
+         <div class="module" style="bottom: 200px;">
+        <farm-module/>
+        </div>
+         <div class="module" style="bottom: 150px; right: 0px;">
+        <farm-module/>
+        </div>
+         <div class="module" style="bottom: 100px; ">
+        <farm-module/>
+        </div>
+      
+    </div>
+</template>
+
+<script>
+import FarmModule from './FarmModule.vue';
+
+    export default {
+  components: { FarmModule },
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+    .module{
+        position: absolute !important;
+    }
+
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <farm-representation/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import FarmRepresentation from '@/components/farm/FarmRepresentation.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
+    FarmRepresentation
   }
 }
 </script>
