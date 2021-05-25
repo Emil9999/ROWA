a474ca8e2ac273b5135a79ee0de8a565ef92c353
Emil9999 <emil.sallem@code.berlin>
2021-05-25 13:24:19 +0200

    bugfix

--- frontend_old/src/router/index.js
+++ frontend_old/src/router/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import Home from '@/views/Home.vue'
+import Farming from "../views/Farming.vue"
 Vue.use(VueRouter)
 
 const routes = [
@@ -7,6 +9,52 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: "/module/:id",
+    name: "ModuleInfo",
+    component: () => import('@/views/Plant')
+  },
+  {
+    path: '/admin',
+    name: 'AdminSettings',
+    component: ()  => import('@/views/AdminSettings')
+
+  },
+  {
+    path: '/pin',
+    name: 'AdminMenu',
+    component: ()  => import('@/views/PinAndAdminMenu')
+
+  },
+  {
+    path: '/adminmodule',
+    name: 'TypeChanger',
+    component: ()  => import('@/views/ModuleTypeSettings')
+
+  },
+
+  {
+    path: '/batchfarming',
+    name: 'BatchFarming',
+    component: () => import('@/views/BatchFarming')
+  },
+  
+  {
+    path: '/plant',
+    name: 'Plant',
+    component: () => import('@/views/Plant')
+  },
+  {
+    path: '/harvest',
+    name: 'Harvest',
+    component: () => import('@/views/Harvest')
+  },
+  {
+    path: '/farming',
+    name: 'Farming',
+    component: Farming
+
   },
 
 ]
