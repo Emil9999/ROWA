5a8aae9da9d3396cdfa1b6a63557802a480409ae
Emil9999 <emil.sallem@code.berlin>
2021-06-03 12:59:33 +0200

    startup light

--- backend/src/main/server.go
+++ backend/src/main/server.go
@@ -71,10 +71,17 @@ func main() {
 	sensor.TriggerPump()
 	time.Sleep(time.Second * 2)
 	sensor.TriggerPump()*/
+	sensor.LightAllOff()
+	time.Sleep(time.Second * 5)
+	sensor.LightAllOn()
+	time.Sleep(time.Second * 1)
+
 	sensor.LightOffModule(2)
 	time.Sleep(time.Second * 10)
 	sensor.LightOnModule(2)
 	sensor.BreathOnModule(2)
+	time.Sleep(time.Second * 5)
+	sensor.BreathOffModule(2)
 
 	e := echo.New()
 
