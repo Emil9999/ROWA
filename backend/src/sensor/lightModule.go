31e1904ef86cbd64b01ba7e2539fd49c98788033
Emil9999 <emil.sallem@code.berlin>
2021-06-10 14:10:00 +0200

    pwm test

--- backend/src/sensor/lightModule.go
+++ backend/src/sensor/lightModule.go
@@ -214,7 +214,7 @@ func SetupLight() {
 }
 
 func PwmTest() {
-	var options *pca9685.Options
+	var options pca9685.Options
 	options.Frequency = 800
 	// Create new connection to i2c-bus on 1 line with address 0x40.
 	// Use i2cdetect utility to find device address over the i2c-bus
