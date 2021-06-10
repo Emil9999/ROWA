86d62ae9c3bf79a2bc6220f236c3c275c989bf7f
Emil9999 <emil.sallem@code.berlin>
2021-06-10 10:49:32 +0200

    pwm test

--- backend/src/sensor/lightModule.go
+++ backend/src/sensor/lightModule.go
@@ -6,6 +6,8 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/googolgl/go-i2c"
+	"github.com/googolgl/go-pca9685"
 	"periph.io/x/conn/v3"
 	"periph.io/x/conn/v3/driver/driverreg"
 	"periph.io/x/conn/v3/gpio"
@@ -208,5 +210,36 @@ func SetupLight() {
 	Modules.Module4 = module4
 	Modules.Module5 = module5
 	Modules.Module6 = module6
+
+}
+
+func PwmTest() {
+	// Create new connection to i2c-bus on 1 line with address 0x40.
+	// Use i2cdetect utility to find device address over the i2c-bus
+	i2c, err := i2c.New(pca9685.Address, 1)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pca0, err = pca9685.New(i2c, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Sets a single PWM channel 0
+	pca0.SetChannel(0, 0, 130)
+
+	// Servo on channel 0
+	light1 := pca0.ServoNew(0, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = light1.SetFreq(1000)
+
+	err = light1.Fraction(0.5)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 }
