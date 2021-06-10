c9e680c7b6081d725704f5b7d969c566e5606848
Emil9999 <emil.sallem@code.berlin>
2021-06-10 16:45:19 +0200

    pwm test

--- backend/src/sensor/lightModule.go
+++ backend/src/sensor/lightModule.go
@@ -13,9 +13,7 @@ import (
 	"periph.io/x/conn/v3/spi"
 	"periph.io/x/conn/v3/spi/spireg"
 	host "periph.io/x/host/v3"
-	"periph.io/x/periph/conn/i2c/i2creg"
 	"periph.io/x/periph/conn/v3/physic"
-	"periph.io/x/periph/experimental/devices/pca9685"
 )
 
 type ModulesStruct struct {
@@ -210,58 +208,6 @@ func SetupLight() {
 	Modules.Module4 = module4
 	Modules.Module5 = module5
 	Modules.Module6 = module6
-
-}
-
-func PwmTest() {
-	/*	options := &pca9685.Options{"pca0", 800.0, 25000000.0}
-
-		// Create new connection to i2c-bus on 1 line with address 0x40.
-		// Use i2cdetect utility to find device address over the i2c-bus
-		i2c, err := i2c.New(pca9685.Address, 1)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		pca0, err := pca9685.New(i2c, options)
-		if err != nil {
-			log.Fatal(err)
-		}
-		//pca0.SetFreq(800.0)
-
-		// Sets a single PWM channel 0
-		pca0.SetChannel(0, 0, 4094)
-
-		// Servo on channel 0
-		light1 := pca0.ServoNew(0, nil)
-		light1.Fraction(1)
-
-		//fmt.Println(pca0.GetFreq())*/
-	_, err := host.Init()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	bus, err := i2creg.Open("")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	pca, err := pca9685.NewI2C(bus, pca9685.I2CAddr)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if err := pca.SetPwmFreq(800); err != nil {
-		log.Fatal(err)
-	}
-	if err := pca.SetAllPwm(0, 0); err != nil {
-		log.Fatal(err)
-	}
-
-	/*if err := pca.SetFullOn(0); err != nil {
-		log.Fatal(err)
-	}*/
 
 }
 
