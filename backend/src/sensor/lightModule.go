c9ac9a48ac80062087e0475d0ed1427a6e325a00
Emil9999 <emil.sallem@code.berlin>
2021-06-10 14:23:30 +0200

    pwm test

--- backend/src/sensor/lightModule.go
+++ backend/src/sensor/lightModule.go
@@ -8,6 +8,8 @@ import (
 
 	"github.com/googolgl/go-i2c"
 	"github.com/googolgl/go-pca9685"
+	"github.com/sergiorb/pca9685-golang/device"
+	"golang.org/x/exp/io/i2c"
 	"periph.io/x/conn/v3"
 	"periph.io/x/conn/v3/driver/driverreg"
 	"periph.io/x/conn/v3/gpio"
@@ -16,6 +18,13 @@ import (
 	"periph.io/x/conn/v3/spi"
 	"periph.io/x/conn/v3/spi/spireg"
 	host "periph.io/x/host/v3"
+)
+
+const (
+	I2C_ADDR  = "/dev/i2c-1"
+	ADDR_01   = 0x40
+	MIN_PULSE = 0
+	MAX_PULSE = 1000
 )
 
 type ModulesStruct struct {
@@ -238,4 +247,31 @@ func PwmTest() {
 
 	//fmt.Println(pca0.GetFreq())
 
+}
+func TestPwm() {
+	i2cDevice, err := i2c.Open(&i2c.Devfs{Dev: I2C_ADDR}, ADDR_01)
+	defer i2cDevice.Close()
+
+	if err != nil {
+
+		mainLog.Error(err)
+
+	} else {
+
+		//var deviceLog = logging.MustGetLogger("PCA9685")
+
+		pca9685 := device.NewPCA9685(i2cDevice, "PWM Controller", MIN_PULSE, MAX_PULSE, deviceLog)
+
+		pca9685.Init()
+
+		pca9685.Demo([]int{0})
+
+		pwm00 := pca9685.NewPwm(0)
+
+		_ = pwm00.SetPercentage(15.0)
+
+		time.Sleep(2 * time.Second)
+
+		//pca9685.SwitchOff([]int{0, 1, 2})
+	}
 }
