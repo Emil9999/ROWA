b4700e6e96250d4f41113091c1bd3a942f2447bb
Emil9999 <emil.sallem@code.berlin>
2021-06-10 14:14:21 +0200

    pwm test

--- backend/src/sensor/lightModule.go
+++ backend/src/sensor/lightModule.go
@@ -214,7 +214,7 @@ func SetupLight() {
 }
 
 func PwmTest() {
-	//options := *pca9685.Options{"pca0", 800, nil}
+	options := *pca9685.Options{"pca0", 800.0, 25000000.0}
 
 	// Create new connection to i2c-bus on 1 line with address 0x40.
 	// Use i2cdetect utility to find device address over the i2c-bus
@@ -223,11 +223,11 @@ func PwmTest() {
 		log.Fatal(err)
 	}
 
-	pca0, err := pca9685.New(i2c, nil)
+	pca0, err := pca9685.New(i2c, options)
 	if err != nil {
 		log.Fatal(err)
 	}
-	pca0.SetFreq(800.0)
+	//pca0.SetFreq(800.0)
 
 	// Sets a single PWM channel 0
 	pca0.SetChannel(0, 0, 130)
