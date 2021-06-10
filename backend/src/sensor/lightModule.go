90192ba9f9046af0c18e1edc1a33f1f68f43260e
Emil9999 <emil.sallem@code.berlin>
2021-06-10 15:15:20 +0200

    pwm test

--- backend/src/sensor/lightModule.go
+++ backend/src/sensor/lightModule.go
@@ -6,9 +6,6 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/op/go-logging"
-	"github.com/sergiorb/pca9685-golang/device"
-	"golang.org/x/exp/io/i2c"
 	"periph.io/x/conn/v3"
 	"periph.io/x/conn/v3/driver/driverreg"
 	"periph.io/x/conn/v3/gpio"
@@ -221,7 +218,7 @@ func SetupLight() {
 
 }
 
-/*func PwmTest() {
+func PwmTest() {
 	options := &pca9685.Options{"pca0", 800.0, 25000000.0}
 
 	// Create new connection to i2c-bus on 1 line with address 0x40.
@@ -246,8 +243,9 @@ func SetupLight() {
 
 	//fmt.Println(pca0.GetFreq())
 
-}*/
-func TestPwm() {
+}
+
+/*func TestPwm() {
 	logger := logging.Logger{}
 	i2cDevice, err := i2c.Open(&i2c.Devfs{Dev: I2C_ADDR}, ADDR_01)
 	defer i2cDevice.Close()
@@ -267,4 +265,4 @@ func TestPwm() {
 
 	//pca9685.SwitchOff([]int{0, 1, 2})
 
-}
+}*/
