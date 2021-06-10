c3aa401dd06af9fd7ba7e4a10e1707f4c47ecb8d
Emil9999 <emil.sallem@code.berlin>
2021-06-10 16:39:02 +0200

    pwm test

--- backend/src/sensor/lightModule.go
+++ backend/src/sensor/lightModule.go
@@ -7,10 +7,10 @@ import (
 	"time"
 
 	"periph.io/x/conn/v3"
-	"periph.io/x/conn/v3//i2c/i2creg"
 	"periph.io/x/conn/v3/driver/driverreg"
 	"periph.io/x/conn/v3/gpio"
 	"periph.io/x/conn/v3/gpio/gpioreg"
+	"periph.io/x/conn/v3/i2c/i2creg"
 	"periph.io/x/conn/v3/physic"
 	"periph.io/x/conn/v3/spi"
 	"periph.io/x/conn/v3/spi/spireg"
