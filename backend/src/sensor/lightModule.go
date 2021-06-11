755cd280991aad47ccd54be2b91262369ad7c3da
Emil9999 <emil.sallem@code.berlin>
2021-06-11 15:44:09 +0200

    piblaster

--- backend/src/sensor/lightModule.go
+++ backend/src/sensor/lightModule.go
@@ -108,6 +108,7 @@ func (lm *Module) BreathOn() {
 				intensityDown = !intensityDown
 			}
 			b.ApplyBlaster(lm.Pin, intensity/100)
+			fmt.Println("writing: " + intensity)
 			time.Sleep(time.Duration(breathingSpeedMs) * time.Millisecond)
 		}
 	}
