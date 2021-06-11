6219092ca4726e933e652dcbbffa03b0ee9b102c
Emil9999 <emil.sallem@code.berlin>
2021-06-11 14:47:38 +0200

    piblaster

--- backend/src/sensor/lightModule.go
+++ backend/src/sensor/lightModule.go
@@ -293,11 +293,11 @@ func BlinkLight(pin int64, toggle bool) {
 	time.Sleep(time.Second * 2)
 	if toggle {
 		for {
-			for i := 12; i < 100; i++ { // increasing brightness
+			for i := 12; i < 70; i++ { // increasing brightness
 				b.ApplyBlaster(pin, float64(i)/100)
 				time.Sleep(time.Millisecond * 20)
 			}
-			for i := 100; i > 11; i-- { // decreasing brightness
+			for i := 70; i > 11; i-- { // decreasing brightness
 				b.ApplyBlaster(pin, float64(i)/100)
 				time.Sleep(time.Millisecond * 20)
 
