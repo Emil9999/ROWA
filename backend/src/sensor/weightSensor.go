cf36548cbece102d139fc65b4fd71c5a87d7a566
Emil9999 <emil.sallem@code.berlin>
2021-06-14 11:40:03 +0200

    scale test

--- backend/src/sensor/weightSensor.go
+++ backend/src/sensor/weightSensor.go
@@ -59,24 +59,23 @@ func TestScale() {
 		return
 	}
 
-	// SetGain default is 128
-	// Gain of 128 or 64 is input channel A, gain of 32 is input channel B
-	// hx711.SetGain(128)
+	err = hx711.Reset()
+	if err != nil {
+		fmt.Println("Reset error:", err)
+		return
+	}
 
-	// make sure to use your values from calibration above
-	hx711.AdjustZero = -123
-	hx711.AdjustScale = 456
-
+	var data int
 	for i := 0; i < 10000; i++ {
 		time.Sleep(200 * time.Microsecond)
 
-		previousReadings := []float64{}
-		movingAvg, err := hx711.ReadDataMedianThenMovingAvgs(11, 8, &previousReadings)
+		data, err = hx711.ReadDataRaw()
 		if err != nil {
-			fmt.Println("ReadDataMedianThenMovingAvgs error:", err)
+			fmt.Println("ReadDataRaw error:", err)
+			continue
 		}
 
-		// moving average
-		fmt.Println(movingAvg)
+		fmt.Println(data)
+	}
 	}
 }
