1557985f7b15314cb76e5aa2ca7bceedd63bb84d
Emil9999 <emil.sallem@code.berlin>
2021-06-14 11:17:24 +0200

    scale test

--- backend/src/sensor/weightSensor.go
+++ backend/src/sensor/weightSensor.go
@@ -53,26 +53,30 @@ func TestScale() {
 		return
 	}
 
-	hx711, err := hx711.NewHx711("GPIO11", "GPIO9")
+	hx711, err := hx711.NewHx711("GPIO6", "GPIO5")
 	if err != nil {
 		fmt.Println("NewHx711 error:", err)
 		return
 	}
 
-	defer hx711.Shutdown()
+	// SetGain default is 128
+	// Gain of 128 or 64 is input channel A, gain of 32 is input channel B
+	// hx711.SetGain(128)
 
-	err = hx711.Reset()
-	if err != nil {
-		fmt.Println("Reset error:", err)
-		return
+	// make sure to use your values from calibration above
+	hx711.AdjustZero = -123
+	hx711.AdjustScale = 456
+
+	var data float64
+	for i := 0; i < 10000; i++ {
+		time.Sleep(200 * time.Microsecond)
+
+		data, err = hx711.ReadDataMedian(11)
+		if err != nil {
+			fmt.Println("ReadDataMedian error:", err)
+			continue
+		}
+
+		fmt.Println(data)
 	}
-
-	time.Sleep(200 * time.Microsecond)
-	previousReadings := []float64{}
-	movingAvg, err := hx711.ReadDataMedianThenMovingAvgs(11, 8, &previousReadings)
-	if err != nil {
-		fmt.Println("ReadDataMedianThenMovingAvgs error:", err)
-	}
-	fmt.Println("Weight", movingAvg)
-
 }
