fa857c1acb5555d65c9f26fadb448709aaee9e15
hannesostwaldcode <hannes.ostwald@code.berlin>
2021-08-04 12:30:10 +0200

    Removed  AirStone Switch

--- frontend/src/components/admin/PumpSettings.vue
+++ frontend/src/components/admin/PumpSettings.vue
@@ -83,20 +83,15 @@
        width="400"
     >
     <v-row align="center"  align-self="center">
-        <v-col cols="5" align="center"  align-self="center">
+      <v-col cols="4"></v-col>
+        <v-col cols="4" align="center"  align-self="center">
      <v-switch large v-model="state_pump" @change="changePump()">
         <template v-slot:label>
         <h3>Pump Switch</h3>
         </template>
       </v-switch>
     </v-col>
-      <v-col cols="5" align="center"  align-self="center">
-     <v-switch large v-model="state_air" @change="changeAir()">
-        <template v-slot:label>
-        <h3>AirStone Switch</h3>
-        </template>
-      </v-switch>
-    </v-col>
+      
     </v-row>
          </v-card>
     </v-row>
@@ -117,7 +112,6 @@ export default {
       time_on: null,
       duration: null,
       state_pump: 0,
-      state_air: 0,
       rules: [
         v => v >= 5 || 'We recommend at least 5 Minutes',
         v => v <= 15 || 'Theres no benefit from running over 15 Minutes',
@@ -167,16 +161,6 @@ export default {
     changePump:function () {
                 axios.post("http://127.0.0.1:3000/adminSettings/changePump",
                     {state: this.state_pump| 0},
-                    "content-type: application/json")
-                    .then()
-                    .catch(error => {
-                        console.log(error);
-                    }); 
-
-            },
-    changeAir:function () {
-                axios.post("http://127.0.0.1:3000/adminSettings/changeAir",
-                    {state: this.state_air| 0},
                     "content-type: application/json")
                     .then()
                     .catch(error => {
