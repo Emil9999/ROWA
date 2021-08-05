a1b3517037606bc836e56a7b28fe7601ef71d111
Hannes Ostwald <hannes.ostwald@code.berlin>
2021-08-05 15:02:16 +0200

    Build Initial Packages and Struct for Frontend2.0

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,27 +1,23 @@
 <template>
-    <v-app id="app">
-        <router-view/>
-    </v-app>
+<div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+  <div class="flex-shrink-0">
+    <img class="h-12 w-12" src="" alt="ChitChatLogo">
+  </div>
+  <div>
+  <div class="text-xl font-medium text-black">ChitChat</div>
+  <p class="text-grey-500">You have a new message</p>
+  </div>
+</div>
 </template>
 
-<script>
-    export default {
-        name: 'App',
-        components: {
-        },
-        data: () => ({
-            //
-        }),
-    };
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {}
+});
 </script>
 
 <style>
-    @import url('https://fonts.googleapis.com/css?family=Montserrat&display=swap');
-
-    #app {
-        background-color: #F7F9F7;
-        font-family: Montserrat,sans-serif;
-        font-style: normal;
-        user-select: none;
-    }
 </style>
