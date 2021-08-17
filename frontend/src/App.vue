6218771bd4d6c6451e121f9d99709a9541641178
Hannes Ostwald <hannes.ostwald@code.berlin>
2021-08-17 09:37:04 +0200

    Test/Experimentation TechStack

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,41 @@
 <template>
+<div>
+  <button class="btn-Susgreen" @click="changeName('Hannes')">Click Me!</button>
 <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
   <div class="flex-shrink-0">
     <img class="h-12 w-12" src="" alt="ChitChatLogo">
   </div>
   <div>
-  <div class="text-xl font-medium text-black">ChitChat</div>
+  <div class="text-xl font-medium text-black">{{name}}</div>
   <p class="text-grey-500">You have a new message</p>
   </div>
+  
+</div>
 </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, reactive, toRefs } from 'vue';
 
 export default defineComponent({
   name: 'App',
-  components: {}
+  components: {},
+  setup() {
+    const state = reactive({
+      name: 'Simon',
+      age: 25 as number | string
+    })
+
+    state.name = 'Emil'
+
+    return {...toRefs(state)}
+  },
+  methods: {
+    changeName(name: string) {
+      this.name = name
+
+    }
+  }
 });
 </script>
 
