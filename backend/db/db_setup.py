95d8ddb56b0afe97f63c299af937133fc70fb659
Emil9999 <emil.sallem@code.berlin>
2022-02-04 14:33:58 +0100

    added setup function for module

--- backend/db/db_setup.py
+++ backend/db/db_setup.py
@@ -1,25 +1,52 @@
-from main import mongo
 import json
-from .schema import Variety
-
-def insert_varieties():
-    with open('data/varieties.json') as json_file:
-            varieties = json.load(json_file)
-            for i in varieties['varieties']:
-                variety = Variety(
-                    name = i['name'],
-                    moving = i['moving'],
-                    harvest_time = i['harvest_time'],
-                    height = i['height'],
-                    size = i['size'],
-                    leaves_harvestable = i['leaves_harvestable'],
-                    harvests_per_week = i['harvests_per_week']
-                )
-                print(variety)
-                variety.update(upsert=True) #insert if not exist or update
+from .schema import Variety, Module
 
 
+def insert_varieties(varieties):
+    # Deleting all entries in varieties and rewriting on every startup
+    Variety.objects().delete()
+    for i in varieties:
+        variety = Variety(
+            name=i['name'],
+            moving=i['moving'],
+            harvest_time=i['harvest_time'],
+            height=i['height'],
+            size=i['size'],
+            leaves_harvestable=i['leaves_harvestable'],
+            harvests_per_week=i['harvests_per_week']
+        )
+        print(variety)
+        variety.save()
+
+
+def insert_modules(total_modules, modules):
+    # Updating existing or creating new modules
+    for i in modules:
+        module = Module(
+            modulenum=i['modulenum'],
+            plant_spots=i['size'],
+            height=i['height'],
+        )
+        print(module.plant_spots)
+        updated_module = Module.objects(modulenum=module.modulenum).modify(upsert=True, new=True,
+                                                                           modulenum=module.modulenum,
+                                                                           plant_spots=module.plant_spots,
+                                                                           height=module.height
+                                                                           )
+        updated_module.save()
+
+    # Deleting dropped modules
+    Module.objects(modulenum__gt=len(modules)).delete()
 
 
 def setup_db():
-    insert_varieties()
+    try:
+        with open('data/farm_data.json') as json_file:
+            farm_data = json.load(json_file)
+            insert_varieties(farm_data['varieties'])
+            insert_modules(farm_data['total_modules'], farm_data['modules'])
+
+    except FileNotFoundError:
+        print("Json Decoding failed")
+
+    
--- backend/db/schema.py
+++ backend/db/schema.py
@@ -1,6 +1,6 @@
 from mongoengine import *
 
-
+#TODO add choices, default, required for the different fields
 class Variety(Document):
     meta = {'collection': 'varieties'}
     name = StringField(primary_key=True)
@@ -14,16 +14,16 @@ class Variety(Document):
 class Plant(EmbeddedDocument):
     variety = ReferenceField(Variety)
     plant_date = DateTimeField()
-    user_name = StringField() #who planted it?
+    user_name = StringField(default = "") #who planted it?
     position = IntField()
     leaves_harvested_total = IntField() #lifetime? 
-    harvests_this_week = IntField() #leaf harvests per week?
-    last_harvest = DateTimeField()
+    harvests_this_week = IntField(default = 0) #leaf harvests per week?
+    last_harvest = DateTimeField(default = None)
 
 class Module(Document):
     meta = {'collection': 'modules'}
-    modulenum = IntField(min_value=1, max_value=6)
-    size = IntField()
-    height = IntField()
+    modulenum = IntField(min_value=1, max_value=50, primary_key=True)
+    plant_spots = IntField(default = 6) #plant spots
+    height = IntField(default = 1) #choice of 1 or 2 for now
     plants = ListField(EmbeddedDocumentField(Plant))
 
