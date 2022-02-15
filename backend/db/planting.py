40659691cdb0f08737f0f5207f95865502831a0e
Emil9999 <emil.sallem@code.berlin>
2022-02-15 15:17:32 +0100

    started planting

--- backend/db/planting.py
+++ backend/db/planting.py
@@ -1,8 +1,18 @@
 from main import mongo
-from .schema import Plant
+from .schema import Plant, Module
 
-def insert_plant(modulenum):
-    module = mongo.db.plants.find_one_and_update({"modulenum": modulenum})
+def insert_plant(content):
+    #print(content['modulenum'])
+    #print(Module.objects(modulenum = content['modulenum'], plantable_varieties = content['variety']))
+    #module = mongo.db.plants.find_one_and_update({"modulenum": modulenum})
     #implement checks if planttype matches, height, size, module not full etc
-    plant = Plant()
-    return "yay"
+    module = Module.objects(modulenum = content['modulenum']).get()
+    print(module.plant_spots)
+    # plants_inmobule = Module.objects(modulenum = content['modulenum'], plants__size = )
+    # if Module.objects(modulenum = content['modulenum'], plantable_varieties = content['variety']) and Module.objects(modulenum = content['modulenum'], plants__size = module.plant_spots):
+    #     #insert plant
+    #     print('success')
+    #     return True
+    # else:
+    #     return False
+    # plant = Plant()
