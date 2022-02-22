5e5abc501006e8012d9990f28577857c99705bae
Emil9999 <emil.sallem@code.berlin>
2022-02-22 21:24:09 +0100

    added harvestable plants

--- backend/db/dashboard.py
+++ backend/db/dashboard.py
@@ -3,14 +3,30 @@ from . import util
 
 
 def get_harvestable_plants():
-    print(harvestable_plants)
+    harvestable_plants = []
+    for module in Module.objects:
+        if util.plants_in_module == 0:
+            continue
+        for planttype in module.plantable_varieties:
+
+            plantable_plant = {
+                "plant_type": planttype.name,
+                "modulenum": module.modulenum
+                #position
+
+            }
+            plantable_plants.append(plantable_plant)
+    print(plantable_plants)   
     return harvestable_plants
+
+def get_harvestable_leaves():
+    return
+
 
 def get_plantable_spots():
     plantable_plants = []
     for module in Module.objects:
         if util.module_full(module.modulenum):
-            print(util.module_full(module.modulenum))
             continue
         for planttype in module.plantable_varieties:
 
